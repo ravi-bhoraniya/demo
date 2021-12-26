37fd3c6bd5b6361b1e15583ca17699011423a284
ravi <ravib@nextgenerationtechnologies.in>
2021-12-26 21:34:08 +0530

    test demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [cart, addToCart] = useState([]);
+  let amount = 0;
+
+  const productlist = [
+    {
+      "_id": "dress1",
+      "title": "Dress 1",
+      "image": "images/dress1.jpeg",
+      "description": "About this dress",
+      "prince": 300,
+      "availableSizes": ["S", "M", "L", "XL"]
+    },
+    {
+      "_id": "dress2",
+      "title": "Dress 2",
+      "image": "images/dress2.jpeg",
+      "description": "About this dress",
+      "prince": 250,
+      "availableSizes": ["S", "M", "L", "XL"]
+    },
+    {
+      "_id": "dress3",
+      "title": "Dress 3",
+      "image": "images/dress3.jpeg",
+      "description": "About this dress",
+      "prince": 350,
+      "availableSizes": ["S", "M", "L", "XL"]
+    },
+    {
+      "_id": "dress4",
+      "title": "Dress 4",
+      "image": "images/dress4.jpeg",
+      "description": "About this dress",
+      "prince": 600,
+      "availableSizes": ["S", "M", "L", "XL"]
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='col-1'>
+        {
+          productlist.map((val, key) => {
+            return (
+              <div className='col'>
+                <div className='cnt'>
+                  <img src={require('./' + val.image)} />
+                  <p>{val.title}</p>
+                  <p>{val.description}</p>
+                  <p>$ {val.prince}</p>
+                  <button onClick={() => addTocartData(val)}>Add to cart</button>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className='col-2'>
+        {
+          cart.map((val, key) => {
+            amount = amount + parseInt(val.prince);
+            return (
+              <div className='col-cart'>
+                <img src={require('./' + val.image)} />
+                <p>{val.title}</p>
+                <p>$ {val.prince}</p>
+                <button onClick={() => removeTocartData(key)}>remove to cart</button>
+              </div>
+            )
+          })
+        }
+        <div className='totalamount'>
+          Total : $ {amount}
+        </div>
+      </div>
     </div>
   );
+
+  function addTocartData(cartValue) {
+    addToCart(oldArray => [...oldArray, cartValue]);
+  }
+
+  function removeTocartData(cartKey) {
+    addToCart(cart.filter((item, key) => key !== cartKey));
+  }
 }
 
 export default App;
